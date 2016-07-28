1c9652d34922fbad6bcfc9170bd00eb726646772
Alejandro Socorro <alejandrosocorro@gmail.com>
2016-07-28 21:51:43 +0200

    First commit.

---
+++ swebs.py
@@ -0,0 +1,10 @@
+import socket
+
+
+HOST, PORT = '', 8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print 'Serving HTTP on port %s ...' % PORT
